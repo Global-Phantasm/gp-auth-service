d7412fa19dec05466fc683e692d7d1a2a1f55869
Dheeraj Kumar <104120915+Dheeraj-Kumar04@users.noreply.github.com>
2024-01-02 19:18:57 +0530

    Initial Service

---
+++ src/test/java/com/gp/pf/auth/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gp.pf.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
